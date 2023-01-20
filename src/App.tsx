94804387ff6a2e048c5d1b7cfd4929e6e9e6dda4
szheron <heronbr7@gmail.com>
2023-01-20 09:46:41 -0300

    Adicionado a tabela de habitos

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { Header } from "./components/Header"
+import { SummaryTable } from "./components/SummaryTable"
+
 function App() {
   return (
-    <div>
-      teste
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl flex flex-col gap-16 ">
+        <Header/>
+        <SummaryTable/>
+      </div>
     </div>
   )
 }
